72f8457a908f5ac9c0caed8c0c3dd4e82714f8c2
epis3559 <pisco.edward@gmail.com>
2016-02-22 09:13:18 -0500

    Lab 1
    Book Exercise

--- Book.java
+++ Book.java
@@ -11,16 +11,79 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
+    private int borrowed;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
-    {
-        author = bookAuthor;
-        title = bookTitle;
+    public Book(String bookAuthor, String bookTitle, int bookPages){
+	author = bookAuthor;
+	title = bookTitle;
+	pages = bookPages;
+	refNumber = "";
     }
 
-    // Add the methods here ...
+   public String getAuthor()
+   {
+       return author;
+    }
+    public String getTitle()
+   {
+       return title;
+       
+    }
+    public int getPages(){
+	return pages;
+    }		
+    public void setRefNumber(String ref){
+	if(ref.length() > 2){
+		refNumber = ref;
+	}
+	else {
+		System.out.println("Error! The reference number must be at least 3 characters long.");
+	}
+    }		
+
+    public String getRefNumber(){
+	return refNumber;
+    }				
+
+    public void printAuthor() 
+    {
+	System.out.println("Author: " + author);
+    }
+    public void printTitle(){
+	System.out.println("Title: " + title);
+    }	
+    
+    public void borrow() {
+	borrowed++;
+    }
+    public int getBorrowed() {
+	return borrowed;
+    }
+
+    public void printDetails(){
+	System.out.println("Title:  " + title);
+	System.out.println("Author: " + author);
+	System.out.println("Pages:  " + pages);
+	System.out.println("Borrowed: " + borrowed);
+	String refNumberString;
+	
+	if(refNumber.length() > 0 ){
+		refNumberString = refNumber;
+	}
+	else {
+		refNumberString = "ZZZ";
+	}
+	System.out.println("Reference number:  " + refNumberString);
+    }
+   
+
+
+   
+
 }
